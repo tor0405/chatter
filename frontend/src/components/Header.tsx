417d79a7c306b8d17565b46edf0d776bc73eb17f
Tor <tortb@stud.ntnu.no>
2019-02-23 13:34:44 +0100

    Added a styled header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.scss'
 
 export const Header: React.FunctionComponent<{}> = () => {
     return (
-        <div>
-            <Link to={"/login"}>Login</Link>
-            <Link to={"/test"}>test</Link>
+        <div className="header">
+            <nav className="header__nav">
+                <Link className="nav__link" to={"/login"}>
+                    <button className="link__button">
+                        Login
+                    </button>
+                </Link>
+                <Link className="nav__link" to={"/register"}>
+                    <button className="link__button">
+                        Register
+                    </button>
+                </Link>
+            </nav>
         </div>
     );
 }
