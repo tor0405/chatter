4bf54b05b14dfc1389847c19100df29f0ae66eff
Tor <tortb@stud.ntnu.no>
2019-02-24 12:21:16 +0100

    fixed docker-error

--- frontend/src/Api.ts
+++ frontend/src/Api.ts
@@ -3,7 +3,7 @@
 export class Api {
 
     static post(url: string, data: object){
-       return fetch('/api', {
+       return fetch('/api'+url, {
             headers: { "Content-Type": "application/json; charset=utf-8" },
             method: 'POST',
             body: JSON.stringify(data)
@@ -17,7 +17,7 @@ export class User extends Api {
 
     static login(username:string, password:string){
         return new Promise(((resolve, reject) => {
-            Api.post("/user/",{username, password})
+            Api.post("/user/login",{username, password})
                 .then(res=>{
                 resolve("Logged in!");
                 console.log(res)})
