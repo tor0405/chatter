ea55ee095f3322fafe426ce02ddd3b2810a72dad
Tor <tortb@stud.ntnu.no>
2019-02-25 16:54:31 +0100

    added info callback to chatsocket

--- frontend/src/Sockets.ts
+++ frontend/src/Sockets.ts
@@ -1,19 +1,23 @@
 import io from 'socket.io-client';
 /*
+
 The socket-classes will not stateless like the api classes, as websockets is not stateless.
+
  */
 
 export class ChatSocket {
     private msgReciever:Function;
+    private infoReciever:Function;
     private socket:SocketIOClient.Socket = io();
     private name:string="";
 
     constructor(chatID:string="", msgReciever:Function=()=>{}, infoReciever:Function=()=>{}){
         this.socket = io('http://localhost:3000/', {path: '/websocket/socket.io',transports:['websocket']});
         this.socket.send(JSON.stringify({"chatID":chatID}));
-        this.msgReciever=msgReciever;
-        this.socket.on("message", this.recieve.bind(this));
-        this.socket.on("connection", this.setName)
+        this.msgReciever=msgReciever;                               //message from another user
+        this.infoReciever=infoReciever;                             // info from the server
+        this.socket.on("message", this.recieveMsg.bind(this));
+        this.socket.on("info", this.infoReciever)
     }
 
     private setName(name:string):void{
@@ -24,8 +28,12 @@ export class ChatSocket {
         return this.name;
     }
 
-    private recieve(msg:string){
+    private recieveMsg(msg:string){
         this.msgReciever(JSON.parse(msg))
+    }
+
+    private recieveInfo(info:string){
+        this.infoReciever(JSON.parse(info))
     }
 
 
