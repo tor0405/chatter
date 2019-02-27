3147caba41fb7a0fc4fd1dbf712e38549e7aab51
Tor <tortb@stud.ntnu.no>
2019-02-27 16:18:11 +0100

    Created a common socket connection

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,6 +1,30 @@
 import io from "socket.io-client";
 
-export default io('ws://localhost:80', {
+let socket = io('ws://localhost:80', {
         path: '/socket/socket.io', transports: ['websocket'],
     },
 );
+
+export default {
+    user:{
+        login(token:string){
+            socket.emit("login", token)
+        }
+    },
+
+    chat:{
+        connect(chatId:string){
+            socket.emit("connect", chatId)
+        },
+        onInfo(callback:Function){
+            socket.on("info", callback);
+        },
+        sendMessage(msg:any){ //TODO:make interface
+          socket.emit("message", msg)
+        },
+        clear(){
+            socket.off("info");
+        }
+    }
+};
+
