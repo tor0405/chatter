7e59582977d7abeb89031022941880c29e6ea9ef
Tor <tortb@stud.ntnu.no>
2019-03-01 17:37:31 +0100

    Created a simple hamburger menu

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,12 +2,15 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.scss'
 import {UserApi} from "../Api";
+import {useState} from "react";
 
 interface Props {
     loggedIn:boolean
 }
 
 export const Header: React.FunctionComponent<Props> = (props) => {
+    const [isActive, setActive] = useState(false);
+
     function loggedIn() {
         return(
             <nav className="header__nav">
@@ -18,12 +21,17 @@ export const Header: React.FunctionComponent<Props> = (props) => {
                         </button>
                     </Link>
                 </section>
-                <section className={"header__right"}>
+                <section className={"header__right "+(isActive?" header__right--active":"")}>
                     <Link className="nav__link" onClick={UserApi.logOut} to={"/"}>
                         <button className="link__button">
                             Logg ut
                         </button>
                     </Link>
+                </section>
+                <section className={"header__mobile-menu"}>
+                    <span onClick={()=>{setActive(!isActive);console.log(isActive)}} className={"menu-button "+(isActive?"is-active":"")} id="menuButton">
+                        <span className="burger-icon"></span>
+                    </span>
                 </section>
             </nav>
         )
@@ -39,7 +47,7 @@ export const Header: React.FunctionComponent<Props> = (props) => {
                         </button>
                     </Link>
                 </section>
-                <section className={"header__right"}>
+                <section className={"header__right "+(isActive?" header__right--active":"")}>
                 <Link className="nav__link" to={"/login"}>
                     <button className="link__button">
                         Login
@@ -50,6 +58,11 @@ export const Header: React.FunctionComponent<Props> = (props) => {
                         Register
                     </button>
                 </Link>
+                </section>
+                <section className={"header__mobile-menu"}>
+                    <span onClick={()=>{setActive(!isActive);console.log(isActive)}} className={"menu-button "+(isActive?"is-active":"")} id="menuButton">
+                        <span className="burger-icon"></span>
+                    </span>
                 </section>
             </nav>
         )
