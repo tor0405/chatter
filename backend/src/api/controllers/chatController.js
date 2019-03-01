ea3795dd8b0c30c590e9c77ac894dbea5f539dbc
Tor <tortb@stud.ntnu.no>
2019-03-01 14:48:54 +0100

    Updated .gitignore

--- backend/src/api/controllers/chatController.js
+++ backend/src/api/controllers/chatController.js
@@ -14,3 +14,15 @@ exports.get = function (req, res) {
 
     });
 };
+
+exports.put = function (req, res) {
+    let token=jwt.decode(req.get("Authorization").split(" ")[1]);
+    Chat.find({participants:{_id:token.id}}, (err, data)=>{
+        if (err) {
+            res.json({'error': err});
+        } else {
+            res.json({chatList:data});
+        }
+
+    });
+};
