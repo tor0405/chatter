b9cb8e136dbfd594f15d4f5feb88b52d36d9451b
Tor <tor0405@hotmail.no>
2019-03-02 14:38:20 +0100

    style: Made site reponsive on phone (#15)

--- frontend/src/views/landing/Landing.tsx
+++ frontend/src/views/landing/Landing.tsx
@@ -8,12 +8,14 @@ export default class Landing extends React.Component {
         return (
             <div className="landing__container">
                 <section className={"landing__intro-container"}>
-                        <h1 className={"landing__intro-header"}>Velkommen til Chatter 🥰</h1>
-                        <h3 className={"landing__intro-sub"}>En elegant, enkel, og litt over-engineered chatteapp.</h3>
+                    <h1 className={"landing__intro-header"}>Velkommen til Chatter 🥰</h1>
+                    <h3 className={"landing__intro-sub"}>En elegant, enkel, og litt over-engineered chatteapp.</h3>
                 </section>
                 <section className={"landing__about"}>
-                    <p className={"about__text"}>Chatter er bygget med React, Typescript, Node, Socket.io, SASS, Docker og Jenkins.</p>
-                    <a href={"https://github.com/tor0405/chatter"}><img className={"about__image"} src={"./github.png"} /></a>
+                    <p className={"about__text"}>Chatter er bygget med React, Typescript, Node, Socket.io, SASS og
+                        Docker.</p>
+                    <a href={"https://github.com/tor0405/chatter"}><img className={"about__image"}
+                                                                        src={"./github.png"}/></a>
                 </section>
             </div>
         );
