bf9f6f9c146e60208d4bb8b7dca32bc5dd4d16b7
Tor <tortb@stud.ntnu.no>
2019-03-03 16:34:35 +0100

    Travis setup

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -x
 
-ssh deploy@192.241.129.61 'bash -s' < ./scripts/build-docker.sh
+ssh -oStrictHostKeyChecking=no deploy@192.241.129.61 'bash -s' < ./scripts/build-docker.sh
