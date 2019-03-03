ce7d95141493797cecb556db9137356e2a8d9c34
Tor <tor0405@hotmail.no>
2019-03-03 17:54:13 +0100

    fix: Fixed a bug where all users recieved all socket.io messages (#16)

--- backend/src/socket/socket.js
+++ backend/src/socket/socket.js
@@ -1,24 +1,27 @@
-let socket = require('socket.io');
+let socket = require("socket.io");
 
 let chat = require("./chat/chat.ts");
 let jwt = require("jsonwebtoken");
 
 module.exports = (app) => {
-    let io = socket(app);
-    io.on("connection", socket => {
-        console.log("New client connected");
-        socket.on("login", (token) => {
-            jwt.verify(token, "Password123", {}, (err, decoded) => { //TODO: env!
-                if (err) {
-                    io.emit("login-event", "false");
-                } else {
-                    io.emit("login-event", "true");
-                    chat(socket, decoded, io);
-                }
-            });
+  let io = socket(app);
+  io.on("connect", socket => {
+    console.log("\n\n\n---------------\n");
+    console.log("hallo");
+    console.log("New client connected");
+    socket.on("login", (token) => {
+      socket.join(socket.id);
+      jwt.verify(token, "Password123", {}, (err, decoded) => { //TODO: env!
+        if (err) {
+          socket.emit("login-event", "false");
+        } else {
+          io.sockets.in(socket.id).emit("login-event", "true");
+          chat(socket, decoded, io);
+        }
+      });
 
-        });
     });
+  });
 
 
 };
